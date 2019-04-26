b245933324e00a30af409484ffb1491e857db04c
Renée Cruz <reneebasscruz@gmail.com>
2019-04-26 21:26:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
+require 'benchmark'
+require 'bigdecimal/math'
+
+# calculate pi to 10k digits
+puts Benchmark.measure { BigMath.PI(10_000) }
+
 def prime?(integer)
     if integer <= 1
       return false
